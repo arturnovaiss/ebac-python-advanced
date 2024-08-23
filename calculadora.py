bde35617ccd18990f31f8727c352b56aa1a02b51
arturnovaiss <arturcomercialpro@gmail.com>
2024-08-23 16:32:34 -0400

    Mudança simples para pull request

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+# Script para calcular a soma de dois números
+
+# Solicita ao usuário que insira o primeiro número
+numero1 = float(input("Digite o primeiro número: "))
+
+# Solicita ao usuário que insira o segundo número
+numero2 = float(input("Digite o segundo número: "))
+
+# Calcula a soma dos dois números
+soma = numero1 + numero2
+
+# Exibe o resultado da soma
+print(f"A soma de {numero1} e {numero2} é {soma}")
